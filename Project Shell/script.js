120cb7e40dc15a6a938e7877b7d50d1cfd62b3cc
dennisbollettino <51206618+dennisbollettino@users.noreply.github.com>
2020-11-30 23:01:37 -0500

    updates

--- Project Shell/script.js
+++ Project Shell/script.js
@@ -1,4 +1,4 @@
-var doc = window 
+var doc = window;
 
  doc.addEventListener('load', function(){
 
@@ -57,8 +57,6 @@ var doc = window
 			var HTML = document.querySelector('#' + CSS.escape(text) + " .text");
 
 		
-		
-			console.log("inside");
 			HTML.innerHTML += "<p class = 'name'>" + name.value + "</p> <p>" + comment.value + "</p";
 
 
@@ -74,41 +72,87 @@ if(document.title == "Arts | Newsn't")
 {
 	var art1F = document.querySelector("#art1F");
 	submitForm(art1F, 'art1');
+
+	var art2F = document.querySelector("#art2F");
+	submitForm(art2F, 'art2');
 }
 
 if(document.title == "Business | Newsn't")
 {
 	var business1F = document.querySelector("#business1F");
 	submitForm(business1F, 'business1');
+
+	var business2F = document.querySelector("#business2F");
+	submitForm(business2F, 'business2');
 }
 
 if(document.title == "Entertainment | Newsn't")
 {
 	var entertainment1F = document.querySelector("#entertainment1F");
 	submitForm(entertainment1F, 'entertainment1');
+
+	var entertainment2F = document.querySelector("#entertainment2F");
+	submitForm(entertainment2F, 'entertainment2');
 }
 
 if(document.title == "Florida Man | Newsn't")
 {
 	var floridaMan1F = document.querySelector("#floridaMan1F");
 	submitForm(floridaMan1F, 'floridaMan1');
+
+	var floridaMan2F = document.querySelector("#floridaMan2F");
+	submitForm(floridaMan2F, 'floridaMan2');
 }
 
 if(document.title == "Politics | Newsn't")
 {
 	var politics1F = document.querySelector("#politics1F");
 	submitForm(politics1F, 'politics1');
+
+	var politics2F = document.querySelector("#politics2F");
+	submitForm(politics2F, 'politics2');
 }
 
 if(document.title == "Science | Newsn't")
 {
 	var science1F = document.querySelector("#science1F");
 	submitForm(science1F, 'science1');
+
+	var science2F = document.querySelector("#science2F");
+	submitForm(science2F, 'science2');
 }
+
+
 
 if(document.title == "Sports | Newsn't")
 {
 	var sports1F = document.querySelector("#sports1F");
 	submitForm(sports1F, 'sports1');
+
+	var sports2F = document.querySelector("#sports2F");
+	submitForm(sports2F, 'sports2');
+
+
 }
 
+
+// picks a random natural disaster for the weather
+
+if(document.title == "Home | Newsn't")
+{
+
+
+	image = document.querySelector('#weather');
+
+	doc.addEventListener('load' , function(){
+
+		imgNum = Math.floor(Math.random() * 5);
+
+		var today = new Date();
+		var day = today.getDay();
+
+
+		image.src = "images/" + day + ".png";
+	});
+
+}
